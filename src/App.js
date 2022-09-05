a29e7f115ac5ab3a0808d9cfa475760ef7125259
ujavaid015 <usama.nexstair@gmail.com>
2022-09-05 18:11:05 +0500

    New Updates

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Tattay Shaat
+          Anday Shaat
         </a>
       </header>
     </div>
